fce6ea5b8cad47cbf06763bb1f25d1ac7355ebe0
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-06 16:24:08 +0200

    added react router to the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import "./App.css"
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Palette from './components/Palette';
-import seedColors from './seedColors';
-import { generatePalette } from './helper/ColorHelpers';
+import Palette from "./components/Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./helper/ColorHelpers";
 
 function App() {
   console.log(generatePalette(seedColors[5]));
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])}/>
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+      <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL PALETTE</h1>}/>
+    </Switch>
+
+    //<div className="App">
+    //<Palette palette={generatePalette(seedColors[4])} />
+    //</div>
   );
 }
 
