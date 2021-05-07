236dbe0f5b0bb2d236d5335ecee56af472e48dd7
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-07 09:26:18 +0200

    created the mini palette and css in js with styles

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 class PaletteList extends Component {
   render() {
-      const { palettes } = this.props;
+    const { palettes } = this.props;
     return (
       <div>
+        <MiniPalette />
         <h1>React Colors</h1>
         {palettes.map((palette) => (
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+          <Link to={`/palette/${palette.id}`} key={palette.id}>
+            {palette.paletteName}
+          </Link>
         ))}
       </div>
     );
